96ac1dffea2a2693e4e5c3f0c3fb6be58652078e
igalv <vilenskyigal@gmail.com>
2021-08-05 15:26:08 +0200

    server, mongo, express

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("App");
+
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = config.get("port") || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+        useNewUrlParser: true,
+        
+    });
+    app.listen(PORT, () => console.log(`App has been started on port ${PORT}`));
+  } catch (e) {
+    console.log("Server Error", e.message);
+    process.exit(1);
+  }
+}
+
+start();
